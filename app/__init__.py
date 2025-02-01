5bf51121a5171e819600d08cefb8741106d4314e
Keshav Swami <swamikeshav18@gmail.com>
2025-02-01 18:02:33 +0530

    check in code structure

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from pymongo import MongoClient
+from .routes import api_blueprint
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # MongoDB connection
+    client = MongoClient(app.config['MONGO_URI'])
+    app.db = client[app.config['DATABASE_NAME']]
+
+    # Register routes
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    return app
