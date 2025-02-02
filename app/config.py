e669feb743721b2df986600445ea369d8dd9d414
Keshav Swami <swamikeshav18@gmail.com>
2025-02-02 12:25:53 +0530

    fix errors in frontend service

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    MONGO_USER = os.getenv("MONGO_USER", "admin")
+    MONGO_USER = os.getenv("MONGO_USER", "admin1")
     MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "password")
-    MONGO_URI = os.getenv("MONGO_URI", f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@localhost:27017/")
+    MONGO_URI = os.getenv("MONGO_URI", f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@localhost:27017/bookstore?authSource=bookstore")
     DATABASE_NAME = os.getenv("DATABASE_NAME", "bookstore")
     APP_PORT = int(os.getenv("APP_PORT", 5000))
-    APP_DEBUG = os.getenv("APP_DEBUG", "false").lower() == "true"
+    APP_DEBUG = os.getenv("APP_DEBUG", "true").lower() == "true"
